edd824ed977a2660c2027c8f06b295add635e1b7
Emanuel Flores <emnl.fmtz@gmail.com>
2021-07-25 00:00:44 -0500

    Added generic local data manager

---
+++ UpperQuizz/DataManager/LocalDataManager.swift
@@ -0,0 +1,40 @@
+//
+//  LocalManager.swift
+//  UpperQuizz
+//
+//  Created by Emanuel Flores Martínez on 24/07/21.
+//
+
+import Foundation
+
+
+final class LocalDataManager {
+    public static func getData<T: Codable>(from jsonFile: String) -> T? {
+        guard let dataJson = self.readLocalJSONFile(forName: jsonFile) else { return nil }
+        guard let dataModel = self.parseModelJSON(of: T.self, from: dataJson) else { return nil }
+        return dataModel
+    }
+    
+    private static func readLocalJSONFile(forName name: String) -> Data? {
+        do {
+            if let filePath = Bundle.main.path(forResource: name, ofType: "json") {
+                let fileURL = URL(fileURLWithPath: filePath)
+                let data = try Data(contentsOf: fileURL)
+                return data
+            }
+        } catch {
+            debugPrint("[-] Error: \(error.localizedDescription)")
+        }
+        return nil
+    }
+    
+    private static func parseModelJSON<T: Codable>(of type: T.Type, from data: Data) -> T? {
+        do {
+            let decodedData: T = try JSONDecoder().decode(T.self, from: data)
+            return decodedData
+        } catch {
+            debugPrint("[-] Error: \(error.localizedDescription)")
+        }
+        return nil
+    }
+}
