249bd99ff2346982956937cdaad9da076da33825
rhodstar <rhodfra@gmail.com>
2021-09-01 16:53:56 -0500

    Added extension to set constraints using the anchor function

---
+++ UpperQuizz/Extensions/UIView+Anchor.swift
@@ -0,0 +1,68 @@
+//
+//  UIView+Anchor.swift
+//  UpperQuizz
+//
+//  Created by Rodrigo Francisco Pablo on 01/09/21.
+//
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                left: NSLayoutXAxisAnchor? = nil,
+                paddingLeft: CGFloat = 0,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                paddingBottom: CGFloat = 0,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingRight: CGFloat = 0,
+                leading: NSLayoutXAxisAnchor? = nil,
+                leadingPadding: CGFloat = 0,
+                trailing: NSLayoutXAxisAnchor? = nil,
+                trailingPadding: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: trailingPadding).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: leadingPadding).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(inView view: UIView) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(inView view: UIView, constant: CGFloat = 0) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+    }
+}
+
